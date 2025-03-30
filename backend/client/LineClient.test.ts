391977af9870d37ccae66aff7797d69c77ec535c
towa1204 <lsadsfj@gmail.com>
2025-03-30 11:08:41 +0900

    test: update tests. t.step -> Deno.test

--- backend/client/LineClient.test.ts
+++ backend/client/LineClient.test.ts
@@ -4,52 +4,46 @@ import { returnsNext, stub } from "@std/testing/mock";
 import { assertRejects } from "@std/assert";
 import { ApiClientError } from "../common/exception.ts";
 
-Deno.test("LineClient", async (t) => {
-  await t.step({
-    name: "actual send",
-    ignore: true,
-    fn: async () => {
-      const lineClient = new LineClient({
-        userid: env("LINE_API_USER_ID"),
-        accessToken: env("LINE_API_TOKEN"),
-      });
-      await lineClient.send("わいわい");
-    },
+Deno.test.ignore("動作確認用のテスト", async () => {
+  const lineClient = new LineClient({
+    userid: env("LINE_API_USER_ID"),
+    accessToken: env("LINE_API_TOKEN"),
   });
+  await lineClient.send("わいわい");
+});
 
-  await t.step("送信できる", async () => {
-    using _ = stub(
-      globalThis,
-      "fetch",
-      returnsNext([
-        Promise.resolve(Response.json("dummy", { status: 200 })),
-      ]),
-    );
+Deno.test("送信できる", async () => {
+  using _ = stub(
+    globalThis,
+    "fetch",
+    returnsNext([
+      Promise.resolve(Response.json("dummy", { status: 200 })),
+    ]),
+  );
 
-    const lineClient = new LineClient({
-      userid: "dummy-user-id",
-      accessToken: "dummy-token",
-    });
+  const lineClient = new LineClient({
+    userid: "dummy-user-id",
+    accessToken: "dummy-token",
+  });
+  await lineClient.send("わいわい");
+});
+
+Deno.test("400エラーのとき例外を送出する", async () => {
+  using _ = stub(
+    globalThis,
+    "fetch",
+    returnsNext([
+      Promise.resolve(Response.json("dummy", { status: 400 })),
+    ]),
+  );
+
+  const lineClient = new LineClient({
+    userid: "dummy-user-id",
+    accessToken: "dummy-token",
+  });
+  const apiClientError = await assertRejects(async () => {
     await lineClient.send("わいわい");
-  });
+  }, ApiClientError);
 
-  await t.step("400エラーのとき例外を送出する", async () => {
-    using _ = stub(
-      globalThis,
-      "fetch",
-      returnsNext([
-        Promise.resolve(Response.json("dummy", { status: 400 })),
-      ]),
-    );
-
-    const lineClient = new LineClient({
-      userid: "dummy-user-id",
-      accessToken: "dummy-token",
-    });
-    const apiClientError = await assertRejects(async () => {
-      await lineClient.send("わいわい");
-    }, ApiClientError);
-
-    console.log(apiClientError.message);
-  });
+  console.log(apiClientError.message);
 });
