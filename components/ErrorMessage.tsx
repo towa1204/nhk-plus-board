0a53d614902bfae3d0cc1ebb909a4ab87e32ba32
towa1204 <lsadsfj@gmail.com>
2025-04-12 16:17:55 +0900

    refactor: extract repeadted function. add ErrorMessage Component

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,16 @@
+/**
+ * エラーメッセージを表示するためのコンポーネント。
+ *
+ * @param message - 表示するエラーメッセージの文字列。
+ * @returns エラーメッセージを含むスタイリングされたアラート要素。
+ */
+export function ErrorMessage({ message }: { message: string }) {
+  return (
+    <div
+      class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      role="alert"
+    >
+      {message}
+    </div>
+  );
+}
